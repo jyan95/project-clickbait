c5be28d6aff394af42677dab2f02b004846060c5
jyan95 <jyan0602@gmail.com>
2019-07-19 15:12:15 -0400

    added template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Page1 from './containers/Page1';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Page1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/content.js
@@ -0,0 +1,28 @@
+const CONTENT = [
+  {
+    title: 'title1',
+    story: 'blah blah'
+  },
+  {
+    title: 'title2',
+    story: 'blah blah'
+  },
+  {
+    title: 'title3',
+    story: 'blah blah'
+  },
+  {
+    title: 'title4',
+    story: 'blah blah'
+  },
+  {
+    title: 'title5',
+    story: 'blah blah'
+  },
+  {
+    title: 'title6',
+    story: 'blah blah'
+  },
+]
+
+export default CONTENT;
